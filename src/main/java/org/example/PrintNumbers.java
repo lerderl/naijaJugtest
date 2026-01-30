edd4b5f26b6a7a524b5a2e666018a6abca1b0c6d
lerderl <josepholukunle1107@gmail.com>
2026-01-30 04:37:44 +0100

    Update main to run PrintNumbers

--- src/main/java/org/example/PrintNumbers.java
+++ src/main/java/org/example/PrintNumbers.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class PrintNumbers {
-  public static void main(String[] args) {
+  public static void print() {
     for (int i = 1; i <= 5; i++) {
       System.out.println("i = " + i);
     }
